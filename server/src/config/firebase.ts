9f057d4dfba12d9e49b950c4b08f277fe65e38b1
BarAppTeam <bar.appteam@gmail.com>
2022-04-06 16:07:46 +0300

    Update Key.json Path

--- server/src/config/firebase.ts
+++ server/src/config/firebase.ts
@@ -2,7 +2,7 @@ import { Firestore } from "@google-cloud/firestore";
 
 const db = new Firestore({
   projectId: "bar-well-b",
-  keyFilename: "keys/bar-well-b-31f0a6b55eb6.json",
+  keyFilename: "keys/key.json",
 });
 
 export default db;
